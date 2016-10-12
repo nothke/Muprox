59770f23187a5612fe8ca1a96c2b2f7904d4aafa
Ivan Notaros <nothke@gmail.com>
2016-10-12 17:29:24 +0200

    Chat works!

--- Assets/Scripts/Chat.cs
+++ Assets/Scripts/Chat.cs
@@ -1,49 +1,76 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.Networking;
+using System.Collections.Generic;
 
 public class Chat : NetworkBehaviour
 {
+
     public string nick = "playerName";
-
-    void Start()
-    {
-
-    }
-
-    void Update()
-    {
-        if (!isLocalPlayer) return;
-
-        if (Input.GetKeyDown(KeyCode.Return))
-            CmdSendChat();
-    }
 
     public string message;
 
-    [SyncVar]
-    public string messages;
+    public static string[] messages = new string[10];
+    public static Queue<string> messageQueue = new Queue<string>();
 
     void OnGUI()
     {
         if (!isLocalPlayer) return;
 
+        if (Event.current.Equals(Event.KeyboardEvent("return")))
+        {
+            CmdSendChat(nick + ": " + message);
+            message = "";
+        }
+
         nick = GUI.TextField(new Rect(10, 200, 100, 30), nick);
         message = GUI.TextField(new Rect(10, 250, 200, 30), message);
 
-        GUI.Label(new Rect(10, 300, 200, 1000), messages);
+        GUI.Label(new Rect(10, 300, 200, 1000), Convert(messageQueue));
+
+
     }
 
     [Command]
-    void CmdSendChat()
+    void CmdSendChat(string msg)
     {
-        RpcSendChat(nick + ": " + message);
+        RpcSendChat(msg);
     }
 
     [ClientRpc]
     void RpcSendChat(string msg)
     {
+        //if (!isLocalPlayer) return;
+
         Debug.Log(msg);
-        messages += "\n" + msg;
+        PushMessage(msg);
+    }
+
+    void PushMessage(string msg)
+    {
+        messageQueue.Enqueue(msg);
+
+        if (messageQueue.Count == 10)
+            messageQueue.Dequeue();
+
+        /*
+        for (int i = 0; i < messages.Length; i++)
+        {
+            if (string.IsNullOrEmpty(messages[i]))
+
+        }*/
+    }
+
+
+    string Convert(Queue<string> lineQueue)
+    {
+        string str = "";
+
+        foreach (var line in lineQueue)
+        {
+            str += "\n" + line;
+        }
+
+        return str;
     }
 }
